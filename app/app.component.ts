e563527bf215ebf5c122d8d475be6b84bfff55d5
Bernardo A. Munoz <bernardoamf@hotmail.com>
2016-10-17 23:11:59 -0700

    Changes: 1. Added courses component 2. Updated app.component to include courses component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { CoursesComponent }   from './courses.component';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: '<h1>My First Angular App2</h1><courses></courses>'
 })
 export class AppComponent { }
